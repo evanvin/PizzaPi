a334fc4aa8cdb61e99fbecb6bc0ff6efd873819a
Evan Vinciguerra <evanv511@gmail.com>
2018-05-17 15:11:46 -0400

    setup script added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from subprocess import call
+cmds = [
+        'sudo add-apt-repository ppa:mc3man/trusty-media',
+        'sudo apt-get update',
+        'sudo apt-get install ffmpeg',
+        'sudo apt install python-pip',
+        'sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style'
+        ]
+
+if __name__ == '__main__':
+    for cmd in cmds:
+        call(cmd)
