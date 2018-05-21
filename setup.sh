6e120b2265ee0c5b723f3c12c43f5fdb48f2dd51
Evan Vinciguerra <evanv511@gmail.com>
2018-05-21 18:54:04 -0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/evanvin/PizzaPi.git
-sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style flask flask-cors
-cd PizzaPi/downloader
