7a420ef7dcc6ccb68c962d435c3df6b04bd3bf07
Evan Vinciguerra <evanv511@gmail.com>
2018-05-21 18:53:21 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/evanvin/PizzaPi.git
-sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style
+sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style flask flask-cors
 cd PizzaPi/downloader
