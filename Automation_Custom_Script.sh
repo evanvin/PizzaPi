ebac85ef16046567d3549a7cfdcd07e920da2286
Evan Vinciguerra <evanv511@gmail.com>
2018-05-21 18:58:58 -0400

    Update Automation_Custom_Script.sh

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -1,9 +1,9 @@
 git clone https://github.com/evanvin/PizzaPi.git
 sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style flask flask-cors
-cd PizzaPi/
-rm -rf dietpi.txt
 cd downloader/
 echo "SPOTIFY={'client_id':'{client_id}','client_secret':'{client_secret}','user':'{user}','playlist':'{playlist}'}" > config.py
 cd ..
 cd storage/
 npm install
+cd ..
+rm -rf dietpi.txt
