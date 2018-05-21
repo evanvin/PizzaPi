d1c3d5b2ce26c26d7d03371c8c3ee692a12b2a85
Evan Vinciguerra <evanv511@gmail.com>
2018-05-21 10:58:05 -0400

    Create Recordings.jsx

---
+++ storage/src/Components/Recordings.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Card,
+  Typography,
+  CardContent,
+  Button,
+  Grid
+} from '@material-ui/core';
+
+class Recordings extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount = async () => {
+    fetch('http://localhost:8008/tracks')
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            data: result
+          });
+        },
+        error => {
+          this.setState({
+            data: []
+          });
+        }
+      );
+  };
+
+  render() {
+    const { data } = this.state;
+    let results = '';
+    if (
+      data === null ||
+      data === 'error' ||
+      data === '' ||
+      data === 'undefined'
+    ) {
+      results = [];
+    } else {
+      results = data;
+    }
+
+    return (
+      <Grid item xs={6}>
+        <Card>
+          <CardContent>
+            <Typography color="secondary" variant="headline" component="h2">
+              Music
+            </Typography>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell />
+                  <TableCell>Artist</TableCell>
+                  <TableCell>Recording</TableCell>
+                  <TableCell />
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {results.map((item, i) => (
+                  <TableRow key={'TableRow_' + i}>
+                    <TableCell>
+                      <img
+                        alt={item.recording}
+                        height="50"
+                        width="50"
+                        src={item.cover}
+                      />
+                    </TableCell>
+                    <TableCell>{item.artist}</TableCell>
+                    <TableCell>{item.recording}</TableCell>
+                    <TableCell>
+                      <Button
+                        href={`http://localhost:8008/download?recording=${
+                          item.recording
+                        }`}
+                      >
+                        Download
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
+}
+
+export default Recordings;
