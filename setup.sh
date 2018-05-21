93465ede6d450829999baed7abdc94fa4c0dfd43
Evan Vinciguerra <evanv511@gmail.com>
2018-05-21 18:12:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-
-sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style gpiozero
-
+git clone https://github.com/evanvin/PizzaPi.git
+sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style
+cd PizzaPi/downloader
