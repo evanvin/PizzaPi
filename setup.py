886a698a4b1ca22b61fd318e08990e58028cadb6
Evan Vinciguerra <evanv511@gmail.com>
2018-05-21 17:51:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from subprocess import call
+#'sudo add-apt-repository ppa:mc3man/trusty-media',
+#'sudo apt-get update',
+#'sudo apt-get install ffmpeg',
+#'sudo apt install python-pip',
 cmds = [
-        'sudo add-apt-repository ppa:mc3man/trusty-media',
-        'sudo apt-get update',
-        'sudo apt-get install ffmpeg',
-        'sudo apt install python-pip',
         'sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style gpiozero'
         ]
 
