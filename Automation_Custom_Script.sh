c3bdc584a7e06301705817cf6868f14a64380ec8
Evan Vinciguerra <evanv511@gmail.com>
2018-05-22 15:21:09 -0400

    Update Automation_Custom_Script.sh

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -11,5 +11,5 @@ rm -rf dietpi.txt
 rm -rf Automation_Custom_Script.sh
 
 cd downloader/
-echo "SPOTIFY={'client_id':'{client_id}','client_secret':'{client_secret}','user':'{user}','playlist':'{playlist}'}" > config.py
+echo "SPOTIFY={'client_id':'{client_id}','client_secret':'{client_secret}','user':'{user}','playlist':'[{playlist}]'}" > config.py
 python python usb.py & main.py
