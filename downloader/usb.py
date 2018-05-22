9e6a7c9ab4cec4814c649f457e2b60ce2caf5ac2
Evan Vinciguerra <evanv511@gmail.com>
2018-05-22 12:47:37 -0400

    Add files via upload

--- downloader/usb.py
+++ downloader/usb.py
@@ -1,5 +1,13 @@
-import os, glob, shutil
-import time
+import os, glob, shutil, time, style
+
+try:
+    from gpiozero import LED
+    red = LED(35)
+    green = LED(47)
+    les_loaded = True
+except Exception as e:
+    leds_loaded = False
+    print(style.light_red.italic('Error importing gpiozero!'))
 
 USB = '/media/eevinciguerra/'
 DOWNLOAD = 'downloads/'
@@ -9,12 +17,16 @@ def checkUSB():
     for d in disks:
         usb_pizza = USB + d + '/pizza/'
         if os.path.exists(usb_pizza):
+            if leds_loaded:
+                green.blink(0.5, 0.5)
             downloaded = [g.replace(DOWNLOAD, "") for g in glob.glob(DOWNLOAD + '*.mp3')]
             pizzas = [g.replace(usb_pizza, "") for g in glob.glob(usb_pizza + '*.mp3')]
             need_to_move = list(set(downloaded) - set(pizzas))
             for f in need_to_move:
                 shutil.copy(DOWNLOAD+f, usb_pizza)
             os.system('sudo eject "' + USB+d + '"')
+            if leds_loaded:
+                green.off()
     time.sleep(5)
     checkUSB()
 
