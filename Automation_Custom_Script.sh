91b00e10b2e45515d5874f2c3845b28189fe0eed
Evan Vinciguerra <evanv511@gmail.com>
2018-05-22 15:56:15 -0400

    Update Automation_Custom_Script.sh

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -9,6 +9,7 @@ cd ../../../
 
 rm -rf dietpi.txt
 rm -rf Automation_Custom_Script.sh
+rm -rf PizzaPi.png
 
 cd downloader/
 echo "SPOTIFY={'client_id':'{client_id}','client_secret':'{client_secret}','user':'{user}','playlist':'[{playlist}]'}" > config.py
