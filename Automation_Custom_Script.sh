5b1a896c6712c1481792feab1ad47789f5143ae7
Evan Vinciguerra <evanv511@gmail.com>
2018-05-22 10:26:45 -0400

    Add files via upload

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -1,9 +1,5 @@
 git clone https://github.com/evanvin/PizzaPi.git
-sudo pip install tinydb spotipy beautifulsoup4 mutagen apscheduler youtube_dl musicbrainzngs style flask flask-cors
-
-cd downloader/
-echo "SPOTIFY={'client_id':'{client_id}','client_secret':'{client_secret}','user':'{user}','playlist':'{playlist}'}" > config.py
-cd ../
+sudo pip install tinydb spotipy beautifulsoup4 mutagen youtube_dl musicbrainzngs style flask flask-cors
 
 cd storage/
 npm install
@@ -14,6 +10,6 @@ cd ../../../
 rm -rf dietpi.txt
 rm -rf Automation_Custom_Script.sh
 
-python downloader/main.py &
-python storage/dataservice.py &
-npm start --prefix storage/
+cd downloader/
+echo "SPOTIFY={'client_id':'{client_id}','client_secret':'{client_secret}','user':'{user}','playlist':'{playlist}'}" > config.py
+python main.py & python usb.py
