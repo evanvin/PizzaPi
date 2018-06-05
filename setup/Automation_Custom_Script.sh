0f6ee93decd2db738a2596250ebfd972ddb71fb1
Evan Vinciguerra <evanv511@gmail.com>
2018-06-05 18:26:01 -0400

    Update Automation_Custom_Script.sh

--- setup/Automation_Custom_Script.sh
+++ setup/Automation_Custom_Script.sh
@@ -1,12 +1,15 @@
 git clone https://github.com/evanvin/PizzaPi.git
 sudo pip install tinydb spotipy beautifulsoup4 mutagen youtube_dl musicbrainzngs style flask flask-cors click
 
-cd PizzaPi/storage/
+cd ~/PizzaPi/storage/
 npm install
 cd src/Components/
 hostname -I | awk '{print "module.exports = {IP:\x27"$1"\x27};"}' > ip.js
 
 cd ~/PizzaPi/downloader/
 cp /boot/config.py ~/PizzaPi/downloader
+
+cd ~/PizzaPi/setup/
+chmod u+x boot.sh
 
 python python usb.py & main.py
