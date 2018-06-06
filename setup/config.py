7b69c5413080a6214de94dfb00104a36d8bd3573
Evan Vinciguerra <evanv511@gmail.com>
2018-06-06 09:17:14 -0400

    Update config.py

--- setup/config.py
+++ setup/config.py
@@ -5,3 +5,9 @@ SPOTIFY = {
   'playlist' : ['playlist_1', 'playlist_2']
 }
 
+
+EMAIL = {
+  'send_ip_on_startup': False,
+  'email_address': '',  # Email address must be a gmail account
+  'email_password': ''
+}
